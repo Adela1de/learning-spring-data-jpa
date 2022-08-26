1218cc4028b299e2742f42e22aee4fb2ee363937
Adela1de <94483883+Adela1de@users.noreply.github.com>
2022-08-26 10:56:47 -0300

    First commit

---
+++ learning-spring-data-jpa-project/src/test/java/com/example/learningspringdatajpaproject/LearningSpringDataJpaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learningspringdatajpaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringDataJpaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
