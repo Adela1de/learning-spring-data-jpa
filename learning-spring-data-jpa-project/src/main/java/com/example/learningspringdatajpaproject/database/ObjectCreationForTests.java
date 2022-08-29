f3e6373157020616246611c9237756ec4efeaae1
Adela1de <94483883+Adela1de@users.noreply.github.com>
2022-08-29 02:40:33 -0300

    initializing objects for database

--- learning-spring-data-jpa-project/src/main/java/com/example/learningspringdatajpaproject/database/ObjectCreationForTests.java
+++ learning-spring-data-jpa-project/src/main/java/com/example/learningspringdatajpaproject/database/ObjectCreationForTests.java
@@ -1,6 +1,9 @@
 package com.example.learningspringdatajpaproject.database;
 
+import com.example.learningspringdatajpaproject.entities.Course;
+import com.example.learningspringdatajpaproject.entities.Guardian;
 import com.example.learningspringdatajpaproject.entities.Student;
+import com.example.learningspringdatajpaproject.entities.Teacher;
 import com.example.learningspringdatajpaproject.repositories.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,12 +24,35 @@ public class ObjectCreationForTests {
 
     public void databaseInit()
     {
-        var student1 = new Student("testname1", "testlastname1", "testeemail1@gmail.com");
-        var student2 = new Student("testname2", "testlastname2", "testeemail2@gmail.com");
-        var student3 = new Student("testname3", "testlastname3", "testeemail3@gmail.com");
-        var student4 = new Student("testname4", "testlastname4", "testeemail4@gmail.com");
-        var student5 = new Student("testname5", "testlastname5", "testeemail5@gmail.com");
+        var s1 = new Student("testname1", "testlastname1", "testeemail1@gmail.com");
+        var s2 = new Student("testname2", "testlastname2", "testeemail2@gmail.com");
+        var s3 = new Student("testname3", "testlastname3", "testeemail3@gmail.com");
+        var s4 = new Student("testname4", "testlastname4", "testeemail4@gmail.com");
+        var s5 = new Student("testname5", "testlastname5", "testeemail5@gmail.com");
 
-        studentRepository.saveAll(Arrays.asList(student1, student2, student3, student4,student5));
+        var g1 = new Guardian("name1", "mailguardian1@gmail.com","000000000");
+        var g2 = new Guardian("name2", "mailguardian2@gmail.com","000000000");
+        var g3 = new Guardian("name3", "mailguardian3@gmail.com","000000000");
+        var g4 = new Guardian("name4", "mailguardian4@gmail.com","000000000");
+        var g5 = new Guardian("name5", "mailguardian5@gmail.com","000000000");
+
+        var c1 = new Course("Course title 1");
+        var c2 = new Course("Course title 2");
+        var c3 = new Course("Course title 3");
+        var c4 = new Course("Course title 4");
+        var c5 = new Course("Course title 5");
+
+        var t1 = new Teacher("Name1", "Nastname1");
+        var t2 = new Teacher("Name3", "Nastname2");
+        var t3 = new Teacher("Name4", "Nastname3");
+        var t4 = new Teacher("Name5", "Nastname4");
+        var t5 = new Teacher("Name6", "Nastname5");
+
+        studentRepository.saveAll(Arrays.asList(s1, s2, s3, s4, s5));
+        guardianRepository.saveAll(Arrays.asList(g1, g2, g3, g4, g5));
+        courseRepository.saveAll(Arrays.asList(c1, c2, c3, c4, c5));
+        teacherRepository.saveAll(Arrays.asList(t1, t2, t3, t4, t5));
+
+
     }
 }
