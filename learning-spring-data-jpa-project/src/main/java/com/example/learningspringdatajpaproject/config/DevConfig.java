f08bfba4bb1e594581d592d92e03bc1059a30481
Adela1de <94483883+Adela1de@users.noreply.github.com>
2022-08-31 09:52:20 -0300

    DevConfig

---
+++ learning-spring-data-jpa-project/src/main/java/com/example/learningspringdatajpaproject/config/DevConfig.java
@@ -0,0 +1,24 @@
+package com.example.learningspringdatajpaproject.config;
+
+import com.example.learningspringdatajpaproject.database.ObjectCreationForTests;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@RequiredArgsConstructor
+@Profile("dev")
+public class DevConfig {
+
+    private final ObjectCreationForTests objectCreationForTests;
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String ddlAuto;
+
+    @Bean
+    public void databaseInitialization()
+    {
+        if(ddlAuto.equalsIgnoreCase("create")) objectCreationForTests.databaseInit();
+    }
+}
